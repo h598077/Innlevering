6f5fde9ff0ec069ea22784cd4ea5d1e315acbf73
Cruizer <Cruizer@158.37.236.67>
2022-09-06 16:42:23 +0200

    Fikset n

--- Levering/src/Dat100/O3.java
+++ Levering/src/Dat100/O3.java
@@ -4,14 +4,19 @@ import static javax.swing.JOptionPane.*;
 import static java.lang.Math.*;
 public class O3 {
 	 static int fakultet(int n){
-	        if (n <= 1 )return 1;  
+	        if (n < 1 )return 1;
 	        return n * fakultet(n -1);
 
 	    }
 	    public static void main(String[] args) {
 	    	String txt = showInputDialog("Skriv n her"); 
 			int n = parseInt(txt); 
-	    	System.out.println(fakultet(n)); 
+			if (n<=0) {
+				System.out.println("n kan ikke vere 0");
+			} else {
+				System.out.println(fakultet(n));
+			}
+	    
 	    }
  
 	}
